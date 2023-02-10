f478e02ab8ff85ee7f26e1a3cb5bdd10aec8cabb
Doma <leishenghao@126.com>
2023-02-10 18:09:36 +0800

    Setup msw integration

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    fetch("/api/user");
+  });
   return (
     <div className="App">
       <header className="App-header">
